0f1c87d4e82abe779c54a1e63a68c4b072228810
genesesLopes <genesesslopes@gmail.com>
2021-01-18 21:25:08 +0000

    criando stubs

---
+++ tests/Feature/Http/Controllers/Api/BasicCrudControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\TestResponse;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Stubs\Controllers\CategoryControllerStub;
+use Tests\Stubs\Models\CategoryStub;
+use Tests\TestCase;
+use Tests\Traits\{
+    TestSaves,
+    TestValidations
+};
+
+
+class BasicCrudControllerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        CategoryStub::dropTable();
+        CategoryStub::createTable();
+    }
+
+    protected function tearDown(): void
+    {
+        CategoryStub::dropTable();
+        parent::tearDown();
+    }
+
+    public function testIndex()
+    {
+        /**@var CategoryStub */ 
+        $category = CategoryStub::create(['name' => 'test_name', 'description' => 'test_description']);
+        $controller = new CategoryControllerStub();
+        $result = $controller->index()->toArray();
+        $this->assertEquals([$category->toArray()],$result);
+    }
+}
