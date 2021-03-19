3f9fd636069207cd561bc641b461db852db3b5f3
genesesLopes <genesesslopes@gmail.com>
2021-03-19 01:17:03 +0000

    testes de sincronizaçao incompleto

--- tests/Feature/Models/VideoTest.php
+++ tests/Feature/Models/VideoTest.php
@@ -20,6 +20,13 @@ class VideoTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
+        $this->data = [
+            'title' => 'title 2',
+            'description' => 'description 2',
+            'year_launched' => 2020,
+            'rating' => Video::RATING_LIST[3],
+            'duration' => 90
+        ];
         $this->video = factory(Video::class)->create();
         $this->video->refresh();
     }
