f12c6ee73ec779644ab3e6a1cfeec538ce8e17ef
geneses.lopes <geneses.lopes.terceirizado@mj.gov.br>
2021-04-02 04:32:12 +0000

    criando testes de produção

---
+++ tests/Prod/Models/Traits/UploadFilesTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Prod\Models\Traits;
+
+use Illuminate\Http\UploadedFile;
+use Tests\Stubs\Models\UploadFileStub;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Storage;
+use Tests\Traits\TestStorages;
+
+class UploadFilesUnitTest extends TestCase
+{
+
+    use TestStorages;
+    /**
+     *
+     * @var UploadFileStub
+     */
+    private $obj;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->obj = new UploadFileStub;
+        \Config::set('filesystem.default', 'gcs');
+        $this->deleteAllFiles();
+    }
+
+    public function testUploadFile()
+    {
+        $file = UploadedFile::fake()->create('video.mp4');
+        $this->obj->uploadFile($file);
+        Storage::assertExists("1/{$file->hashName()}");
+    }
+
+    public function testUploadFiles()
+    {
+        $files = [];
+        for ($i = 1; $i <= 2; $i++)
+            array_push($files, UploadedFile::fake()->create("video{$i}.mp4"));
+        $this->obj->uploadFiles($files);
+        foreach ($files as $file) {
+            Storage::assertExists("1/{$file->hashName()}");
+        }
+    }
+
+
+    public function testDeleteFile()
+    {
+        $file = UploadedFile::fake()->create('video.mp4');
+        $this->obj->uploadFile($file);
+        $this->obj->deleteFile($file->hashName());
+        Storage::assertMissing("1/{$file->hashName()}");
+
+        $file = UploadedFile::fake()->create('video.mp4');
+        $this->obj->uploadFile($file);
+        $this->obj->deleteFile($file);
+        Storage::assertMissing("1/{$file->hashName()}");
+    }
+
+    public function testDeleteOldFiles()
+    {
+        $files = [];
+        for ($i = 1; $i <= 2; $i++)
+            array_push($files, UploadedFile::fake()->create("video{$i}.mp4")->size(1));
+        $this->obj->uploadFiles($files);
+        $this->obj->deleteOldFiles();
+        $this->assertCount(2, Storage::allFiles());
+
+        $this->obj->oldFiles = [$files[1]->hashName()];
+        $this->obj->deleteOldFiles();
+        foreach ($files as $key => $file) {
+            if ($key)
+                Storage::assertMissing("1/{$file->hashName()}");
+            else
+                Storage::assertExists("1/{$file->hashName()}");
+        }
+    }
+
+    public function testDeleteFiles()
+    {
+        $files = [];
+        for ($i = 1; $i <= 2; $i++)
+            array_push($files, UploadedFile::fake()->create("video{$i}.mp4"));
+        $this->obj->uploadFiles($files);
+        $this->obj->deleteFiles([$files[0]->hashName(), $files[1]]);
+        foreach ($files as $file) {
+            Storage::assertMissing("1/{$file->hashName()}");
+        }
+    }
+}
