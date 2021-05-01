09f7fbb79fbd2900ba0df64cd50b0bc6e33d272c
lopesgeneses <geneses.lopes@ext.webedia-group.com>
2021-05-01 12:42:21 -0300

    correção de link simbólico

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -12,6 +12,12 @@ fi
 if [ ! -f ".env.testing" ]; then
     cp .env.testing.example .env.testing
 fi
+
+if [ ! -L "public/storage" ]; then
+    cd public && ln -s /var/www/backend/storage/app/public storage
+    cd ../
+fi
+
 chown -R www-data:www-data .
 composer install
 php artisan key:generate
