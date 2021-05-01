d6f3dc02d89e58188a1d4327ad3b44889e8f0c1f
lopesgeneses <geneses.lopes@ext.webedia-group.com>
2021-05-01 14:40:09 -0300

    estilizando navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,10 +1,32 @@
-import { AppBar, Toolbar } from '@material-ui/core';
 import * as React from 'react';
+import { AppBar, Button, makeStyles, Theme, Toolbar, Typography } from '@material-ui/core';
+import logo from '../../assets/img/logo.png';
+
+const useStyles = makeStyles((theme: Theme) => ({
+    toolbar: {
+        backgroundColor: '#000000'
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'center'
+    },
+    logo: {
+        width: 100,
+        [theme.breakpoints.up('sm')]:{
+            width: 170
+        }
+    }
+}))
+
 export const Navbar: React.FC = () => {
+    const classes = useStyles()
     return (
        <AppBar>
-           <Toolbar>
-               Texto
+           <Toolbar className={classes.toolbar}>
+               <Typography className={classes.title}>
+                   <img src={logo} alt="Codeflix" className={classes.logo}/>
+               </Typography>
+               <Button color='inherit'>Login</Button>
            </Toolbar>
         </AppBar>
     );
