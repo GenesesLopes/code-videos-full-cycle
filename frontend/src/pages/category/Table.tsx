2b7dc1c9bec7949c984cf444c0cf969ff1a8265f
lopesgeneses <geneses.lopes@ext.webedia-group.com>
2021-06-03 20:19:30 -0300

    configurando axios e cors

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MUIDataTable, { MUIDataTableColumn } from 'mui-datatables';
+import { httpVideo } from '../../utils/http';
 
 const colunsDefinitions: MUIDataTableColumn[] = [
     {
@@ -16,21 +17,33 @@ const colunsDefinitions: MUIDataTableColumn[] = [
     }
 ];
 
-const data = [
-    {
-        name: 'Teste',
-        is_active: true,
-        created_at: '2019-12-12'
-    },
-    {
-        name: 'Teste2',
-        is_active: false,
-        created_at: '2020-12-12'
-    }
-];
+// const data = [
+//     {
+//         name: 'Teste',
+//         is_active: true,
+//         created_at: '2019-12-12'
+//     },
+//     {
+//         name: 'Teste2',
+//         is_active: false,
+//         created_at: '2020-12-12'
+//     }
+// ];
 
 type Props = {};
 const Table = (props: Props) => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        httpVideo.get('categories').then(response =>{
+            setData(response.data.data)
+        }).catch(error => {
+            console.error(error)
+        })
+        console.log('qualquer coisa')
+    },[])
+    
     return (
         <MUIDataTable 
             title='Listagem de Categorias'
