6fbafe968cfd98d3a55e16a710d5ca6eb600c1ad
lopesgeneses <geneses.lopes@ext.webedia-group.com>
2021-06-09 00:55:59 -0300

    instalando e configurando react hocks

--- frontend/src/pages/category/Form.tsx
+++ frontend/src/pages/category/Form.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Box, Button, ButtonProps, Checkbox, makeStyles, TextField, Theme } from '@material-ui/core';
+import { useForm, Controller } from 'react-hook-form';
 
 const useStyles = makeStyles((theme: Theme) => {
     return {
@@ -18,29 +19,51 @@ const Form = () => {
         className: classes.submit
     }
 
+    const { getValues, control } = useForm()
+
     return (
         <form>
-            <TextField 
-             name='name'
-             label='Nome'
-             fullWidth
-             variant={'outlined'}
+            <Controller
+                name='name'
+                control={control}
+                defaultValue=""
+                render={
+                    ({ field }) => <TextField
+                        {...field}
+                        label='Nome'
+                        fullWidth
+                        variant={'outlined'}
+                    />
+                }
             />
-            <TextField 
-             name='description'
-             label='Descrição'
-             multiline
-             rows='4'
-             fullWidth
-             variant={'outlined'}
-             margin={'normal'}
+            <Controller
+                name='description'
+                control={control}
+                defaultValue=""
+                render={
+                    ({ field }) => <TextField
+                        {...field}
+                        label='Descrição'
+                        multiline
+                        rows='4'
+                        fullWidth
+                        variant={'outlined'}
+                        margin={'normal'}
+                    />
+                }
+
             />
-            <Checkbox
-             name='is_active'
+            <Controller 
+                name='is_active'
+                control={control}
+                defaultValue={false}
+                render={
+                    ({ field }) => <Checkbox {...field} />
+                }
             />
             Ativo ?
             <Box dir='rtl'>
-                <Button {...buttonProps}>Salvar</Button>
+                <Button {...buttonProps} onClick={() => console.log(getValues())}>Salvar</Button>
                 <Button {...buttonProps} type='submit'>Salvar e continuar editando</Button>
             </Box>
         </form>
